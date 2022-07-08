b3f50a6be4870f4b68ce4f19ddff168e6ea37118
rPrinceA495 <princepappiah@gmail.com>
2022-07-08 01:07:15 +0000

    ensure backend and frontend exist

--- .circleci/files/create-backend.sh
+++
@@ -1,5 +0,0 @@
-aws cloudformation deploy \
-              --template-file backend.yml \
-              --stack-name "udapeople-backend-${CIRCLE_WORKFLOW_ID:0:7}" \
-              --parameter-overrides ID="${CIRCLE_WORKFLOW_ID:0:7}" \
-              --tags project=udapeople
