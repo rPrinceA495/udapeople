0ff39c64dd83474588c7ed2afc182400810509ed
rPrinceA495 <princepappiah@gmail.com>
2022-07-18 02:27:14 +0000

    prometheus node export

--- .circleci/files/prometheus.sh
+++ .circleci/files/prometheus.sh
@@ -1,3 +1,3 @@
-aws cloudformation deploy \
-         --template-file prometheus-server.yml \
-         --stack-name PrometheusStack
+aws cloudformation deploy --stack-name prometheus --template-file prometheus-server.yml --capabilities CAPABILITY_IAM \
+  --parameter-overrides AuthEmail=rpa41995@gmail.com EmailPassword=iwqfzolasbagwsya FromEmail=rpa41995@gmail.com ToEmail=rpa41995@gmail.com \
+  VPC=vpc-08eca57e24273808e AZ=us-east-1a Subnet=subnet-0e400092f8c4a813a
